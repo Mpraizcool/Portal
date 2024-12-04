0b4cab3f6b162f6e737a99bac558408e1eb7b5ba
Olaleye Praise <65808506+Mpraizcool@users.noreply.github.com>
2024-12-04 20:11:50 +0100

    Update register.php
    NO CSS

--- register.php
+++ register.php
@@ -28,7 +28,7 @@ if($query)
 
 	<body class="login">
 		<!-- start: REGISTRATION -->
-         <h1>NO VEX MY FANS... THE CSS NEVA  SHOW</h1>
+         
 		<div class="row">
 			<div class="main-login col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4">
 				<div class="logo margin-top-30">
